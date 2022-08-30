71ff51a112bdd7d6501ca01e8bd5fc1279117061
AhmedNasserG <ahmednasser217217@gmail.com>
2022-08-30 13:00:09 +0200

    Update CourseCard

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -5,27 +5,30 @@ import './CourseCard.css';
 
 const CourseCard = (props) => {
 	return (
-		<div className="course-card">
-			<img src={props.course.image} alt={props.course.title} className="course-img"></img>
-			<div className="course-card-body">
-				<p className="course-title">{props.course.title}</p>
-				<p className="course-subtitle">{props.course.instructor}</p>
-				<div className="course-rating">
-					<p className="course-rating-text">{props.course.rating}</p>
+		<div className='course-card'>
+			<img
+				src={props.course.image}
+				alt={props.course.title}
+				className='course-img'
+			></img>
+			<div className='course-card-body'>
+				<p className='course-title'>{props.course.title}</p>
+				<p className='course-subtitle'>{props.course.instructor}</p>
+				<div className='course-rating'>
+					<p className='course-rating-text'>{props.course.rating}</p>
 					<StarRatings
 						rating={props.course.rating}
-						starDimension="14px"
-						starSpacing="1px"
-						starRatedColor="#E59719"
+						starDimension='14px'
+						starSpacing='1px'
+						starRatedColor='#E59719'
 					/>
-					<p className="course-student-count">({props.course.studentCount})</p>
+					<p className='course-student-count'>({props.course.studentCount})</p>
 				</div>
-				<p className="course-price">{props.course.price}</p>
+				<p className='course-price'>{props.course.price}</p>
 
 				{props.course.bestSeller && (
-					<div className="best-seller">
-						{' '}
-						<p className="best-seller-text">Bestseller</p>
+					<div className='best-seller'>
+						<p className='best-seller-text'>Bestseller</p>
 					</div>
 				)}
 			</div>
