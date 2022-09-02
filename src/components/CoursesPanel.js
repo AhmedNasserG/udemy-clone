5beab997cee368884c0e1dd12de0e58d316fdc99
AhmedNasserG <ahmednasser217217@gmail.com>
2022-09-02 11:45:47 +0200

    refactor: extract CoursesPanel component

--- src/components/CoursesPanel.js
+++ src/components/CoursesPanel.js
@@ -1,61 +1,21 @@
 import React from 'react';
-import CourseCard from './CourseCard';
-import Carousel from 'react-multi-carousel';
-import './CardsSlider.css';
-import './CourseCard.css';
-import 'react-multi-carousel/lib/styles.css';
+import CoursesSlider from './CoursesSlider';
+import './CoursesPanel.css';
 
-const responsive = {
-	superLargeDesktop: {
-		breakpoint: { max: 3000, min: 1368 },
-		items: 5,
-		slidesToSlide: 5,
-	},
-	desktop: {
-		breakpoint: { max: 1368, min: 1098 },
-		items: 4,
-		slidesToSlide: 4,
-	},
-	tablet: {
-		breakpoint: { max: 1098, min: 844 },
-		items: 3,
-		slidesToSlide: 3,
-	},
-	mobile: {
-		breakpoint: { max: 944, min: 593 },
-		items: 2,
-		slidesToSlide: 2,
-	},
-	mobile1: {
-		breakpoint: { max: 593, min: 0 },
-		items: 1,
-		slidesToSlide: 1,
-	},
-};
-
-const CardsSlider = ({ title, description, courses }) => {
+const CoursesPanel = ({ category, title, description, courses }) => {
 	return (
 		<div className='courses-container'>
 			<h2>{title}</h2>
 			<p className='category-description'>{description}</p>
-			<button className='explore-button'>Explore Python</button>
-			<div className='cards-slider'>
-				<Carousel
-					responsive={responsive}
-					containerClass='cards-slider'
-					itemClass='course-card'
-				>
-					{courses.map((course) => (
-						<CourseCard key={course.id} course={course} />
-					))}
-				</Carousel>
-			</div>
+			<button className='explore-button'>Explore {category}</button>
+			<CoursesSlider courses={courses} />
 		</div>
 	);
 };
 
 // for testing purposes
-CardsSlider.defaultProps = {
+CoursesPanel.defaultProps = {
+	category: 'Python',
 	title: 'Expand your career opportunities with Python',
 	description: `Take one of Udemy’s range of Python courses and learn how to code using this incredibly
 	      useful language. Its simple syntax and readability makes Python perfect for Flask, Django,
@@ -114,7 +74,7 @@ CardsSlider.defaultProps = {
 			bestSeller: false,
 		},
 		{
-			id: 1,
+			id: 6,
 			image: 'https://s.udemycdn.com/premium-clp/567828/CourseImage-2x.jpg',
 			title: 'python from zero to hero',
 			instructor: 'maximlian',
@@ -124,7 +84,7 @@ CardsSlider.defaultProps = {
 			bestSeller: true,
 		},
 		{
-			id: 2,
+			id: 7,
 			image:
 				'https://www.pragimtech.com/wp-content/uploads/2019/03/java-script.jpg',
 			title: 'javascript from zero to hero',
@@ -135,7 +95,7 @@ CardsSlider.defaultProps = {
 			bestSeller: true,
 		},
 		{
-			id: 3,
+			id: 8,
 			image: 'https://img-c.udemycdn.com/course/240x135/426570_1b91_3.jpg',
 			title: 'Learn Python: Python for Beginners',
 			instructor: 'Abrar Hussain',
@@ -145,7 +105,7 @@ CardsSlider.defaultProps = {
 			bestSeller: false,
 		},
 		{
-			id: 4,
+			id: 9,
 			image: 'https://img-c.udemycdn.com/course/240x135/396876_cc92_7.jpg',
 			title: 'Learn Python for Data Analysis and Visualization',
 			instructor: 'Jose Portila',
@@ -155,7 +115,7 @@ CardsSlider.defaultProps = {
 			bestSeller: true,
 		},
 		{
-			id: 5,
+			id: 10,
 			image: 'https://img-c.udemycdn.com/course/240x135/405878_e5a0_3.jpg',
 			title: 'Python for Beginners - Learn Programming from scratch',
 			instructor: 'Edwin Diaz, Coding Faculty',
@@ -167,4 +127,4 @@ CardsSlider.defaultProps = {
 	],
 };
 
-export default CardsSlider;
+export default CoursesPanel;
