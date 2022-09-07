2d5dbb7001c1d2ca67ada5f11cb65e0372958086
AhmedNasserG <ahmednasser217217@gmail.com>
2022-09-07 16:31:29 +0200

    feat: add initial routing set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import CoursePage from './course-page/CoursePage';
 import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
-			<Home />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='course' element={<CoursePage />} />
+			</Routes>
 		</div>
 	);
 }
